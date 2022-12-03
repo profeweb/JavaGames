de22d2333d7d92a876119879212bca9fd83ec1cc
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-12-03 15:05:21 +0100

    magic sqaure

---
+++ src/gameOfCraps/GameOfCraps.java
@@ -0,0 +1,10 @@
+package gameOfCraps;
+
+public class GameOfCraps {
+
+	public static void main(String[] args) {
+		CrapsGame craps = new CrapsGame();
+        craps.play();
+	}
+
+}
--- src/gameofcraps/gameofcraps.java
+++
@@ -1,4 +0,0 @@
-package gameofcraps;
-
-public class gameofcraps {
-}
